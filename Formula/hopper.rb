aa23569be95145bd2010a950036272a2de27958e
Bharath A V <avbharath1221@gmail.com>
2026-07-22 12:32:32 +0530

    chore: update formula to v1.2.0

--- Formula/hopper.rb
+++ Formula/hopper.rb
@@ -3,20 +3,20 @@ require_relative "../custom_download_strategy"
 class Hopper < Formula
   desc "nammayatri build network CLI"
   homepage "https://github.com/nammayatri/hopper"
-  version "1.1.0"
+  version "1.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nammayatri/hopper/releases/download/v1.1.0/hopper-darwin-arm64",
+      url "https://github.com/nammayatri/hopper/releases/download/v1.2.0/hopper-darwin-arm64",
           using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "76d80fcf461b89513a525ff2a9a2b28e5f32a1b1dfdaaa5367c424306bea50a9"
+      sha256 "90820f75616347f73eeccff9c36cbbda61a2cf3dce3311164987573722f3d9e6"
     end
   end
 
   on_linux do
-    url "https://github.com/nammayatri/hopper/releases/download/v1.1.0/hopper-linux-amd64",
+    url "https://github.com/nammayatri/hopper/releases/download/v1.2.0/hopper-linux-amd64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "ef545e1c2f13f4b564f7a3f2da9c4075e0c0209241d8918112969bcbeb139b96"
+    sha256 "f9984a05398ba15699def3c849e4b48aa84cc0ee6d3a5c1b6e8e66f2f1396865"
   end
 
   def install
