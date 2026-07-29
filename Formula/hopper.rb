d600f290015ce23595d0d407902a7a9112c030d4
Bharath A V <avbharath1221@gmail.com>
2026-07-29 16:10:20 +0530

    feat: bump hopper to v1.3.0

--- Formula/hopper.rb
+++ Formula/hopper.rb
@@ -3,20 +3,20 @@ require_relative "../custom_download_strategy"
 class Hopper < Formula
   desc "nammayatri build network CLI"
   homepage "https://github.com/nammayatri/hopper"
-  version "1.2.0"
+  version "1.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nammayatri/hopper/releases/download/v1.2.0/hopper-darwin-arm64",
+      url "https://github.com/nammayatri/hopper/releases/download/v1.3.0/hopper-darwin-arm64",
           using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "90820f75616347f73eeccff9c36cbbda61a2cf3dce3311164987573722f3d9e6"
+      sha256 "4afa3221dcbcd2b0ec5f3ddab7df7d039205dd6f9a852954bfbf50d132a62db7"
     end
   end
 
   on_linux do
-    url "https://github.com/nammayatri/hopper/releases/download/v1.2.0/hopper-linux-amd64",
+    url "https://github.com/nammayatri/hopper/releases/download/v1.3.0/hopper-linux-amd64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "f9984a05398ba15699def3c849e4b48aa84cc0ee6d3a5c1b6e8e66f2f1396865"
+    sha256 "33a422c34b91be34c608160f09cee9f6d77920cac97892148ff64f1322ccb088"
   end
 
   def install
